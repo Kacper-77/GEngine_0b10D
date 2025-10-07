c5e1ab49388b58f30301823953a7406f83038b1b
Kacper-77 <ks.kontaktowy7@gmail.com>
2025-10-07 19:54:48 +0200

    main tested

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "core/ComponentStorage.h"
 #include "components/TransformComponent.h"
 #include "components/VelocityComponent.h"
+#include "components/HealthComponent.h"
 #include "systems/MovementSystem.h"
 #include "systems/RenderSystem.h"
 #include "systems/EntityCreationSystem.h"
@@ -31,11 +32,64 @@ int main(int argc, char* argv[]) {
 
     renderer.SetDrawColor(30, 30, 60, 255);
 
-    // Texture
+    // ECS setup
+    EntityManager entityManager;
+    EntityCreationSystem creationSystem{&entityManager};
+
+    ComponentStorage<TransformComponent> transforms;
+    ComponentStorage<VelocityComponent> velocities;
+    ComponentStorage<AccelerationComponent> accelerations;
+    ComponentStorage<SpriteComponent> sprites;
+
+    creationSystem.RegisterStorage(&transforms);
+    creationSystem.RegisterStorage(&velocities);
+    creationSystem.RegisterStorage(&accelerations);
+    creationSystem.RegisterStorage(&sprites);
+
+    entityManager.RegisterComponentStorage(&transforms);
+    entityManager.RegisterComponentStorage(&velocities);
+    entityManager.RegisterComponentStorage(&accelerations);
+    entityManager.RegisterComponentStorage(&sprites);
+
+    // Load texture
     Texture playerTexture;
     if (!playerTexture.LoadFromFile("../assets/fish_brown.png", renderer.GetSDLRenderer())) {
         std::cerr << "Failed to load player texture." << std::endl;
         return -1;
+    }
+
+    // Create entity
+    EntityID player = creationSystem.CreateEntityWith(
+        TransformComponent{364, 100, 64, 64},
+        VelocityComponent{2.0f, 1.0f},
+        AccelerationComponent{0.0f, 0.0f},
+        SpriteComponent{&playerTexture, 64, 64}
+    );
+
+    MovementSystem movementSystem(transforms, velocities, accelerations);
+    RenderSystem renderSystem(transforms, sprites, &renderer);
+    SystemManager systemManager;
+    systemManager.RegisterSystem<MovementSystem>(movementSystem);
+
+    InputManager inputManager;
+    inputManager.Bind("Left", SDL_SCANCODE_LEFT);
+
+    auto transform = transforms.Get(player);
+
+    while (window.IsRunning()) {
+        window.PollEvents();
+        inputManager.Update();
+
+        auto velocity = velocities.Get(player);
+
+        if (inputManager.IsActionHeld("Left")) velocity->dx -= 1;
+
+        systemManager.UpdateAll(1.0f);
+        renderer.Clear();
+        renderSystem.Update(1.0f);
+        renderer.Present();
+
+        SDL_Delay(1000 / 60); // ~60 FPS
     }
 
     playerTexture.Unload();
